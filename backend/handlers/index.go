0a61106b4ecdf9e59cbd23cdb444688db4a019fc
Linnea Gabrielsson <annalinneagabrielsson@gmail.com>
2025-03-26 12:09:15 +0200

    removed debugging

--- backend/handlers/index.go
+++ backend/handlers/index.go
@@ -2,21 +2,8 @@ package handlers
 
 import (
 	"net/http"
-	"os"
-	"path/filepath"
 )
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	// Add cache-busting headers
-	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
-	w.Header().Set("Pragma", "no-cache")
-	w.Header().Set("Expires", "0")
-
-	// Serve the index.html file
-	wd, err := os.Getwd()
-	if err != nil {
-		http.Error(w, "500 internal server error", http.StatusInternalServerError)
-		return
-	}
-	http.ServeFile(w, r, filepath.Join(wd, "frontend", "index.html"))
+	http.ServeFile(w, r, "./frontend/index.html")
 }
